125f059cb5f9bbd4c367acc152d72683bbaf431d
Azahra Rahma Desfajrika <69596971+Arasaurus@users.noreply.github.com>
2022-02-08 13:57:16 +0700

    Main program

---
+++ Main program/appCam.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# appCam.py
+# Local Web Server with Flask
+
+from flask import Flask, render_template, Response, request, redirect, url_for, make_response
+import time
+import os
+from camera_pi import Camera
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    timeNow = time.asctime( time.localtime(time.time()) )
+    templateData = {
+      'time': timeNow,
+    }
+    """Video streaming home page."""
+    return render_template('index.html')
+
+@app.route('/lock')
+def lock():
+    """selonoid page."""
+    os.system("python selonoid_lock.py")
+    return render_template('selonoid.html')
+
+@app.route('/unlock')
+def unlock():
+    """selonoid page."""
+    os.system("python selonoid_lock.py ")
+    return render_template('selonoid.html')
+
+@app.route('/<changepin>', methods=['POST'])
+def key():
+    if request.method == 'POST':
+    response = make_response(redirect(url_for('selonoid')))
+    return(response)
+    return render_template('selonoid.html')
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port =5000, debug=True, threaded=True)
